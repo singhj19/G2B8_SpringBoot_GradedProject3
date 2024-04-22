fbc24bc07da240ca23c014d5e66b8bde8b6ead58
Jitendra Singh <mailforjitendra91@gmail.com>
2024-04-22 13:53:04 +0530

    Project Structure

---
+++ src/main/java/com/g2b8/tta/g2b8_bed_gradedproject3/G2B8BedGradedProject3Application.java
@@ -0,0 +1,13 @@
+package com.g2b8.tta.g2b8_bed_gradedproject3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G2B8BedGradedProject3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(G2B8BedGradedProject3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/g2b8/tta/g2b8_bed_gradedproject3/G2B8BedGradedProject3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.g2b8.tta.g2b8_bed_gradedproject3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G2B8BedGradedProject3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
